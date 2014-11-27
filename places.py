19ef301b4564244380b67fd26275444aff82cf4f
sirspinach <wang.steven.h@gmail.com>
2014-11-26 21:21:54 -0800

    completely remake Place, Waypoint Place becomes Somewhere, Waypoint becomes Place

--- places.py
+++ places.py
@@ -1,10 +1,10 @@
 from dialogue import *
 from decisions import *
 class World(object):
-    """A World is a dictionary-like collection of related Places. Since the same 'place' in the game
-    is actually one of several different Place instances depending on the state of the World,
-    (e.g. night or day), it is best that a Place store a exit as a DelayedAccesses pointing
-    to a World attribute rather than a pointer to any particular Place."""
+    """A World is a dictionary-like collection of Somewheres. Since the same 'place' in the game
+    can be one of several different Somewhere instances depending on the state of the World,
+    (e.g. night or day), it is best that a Somewhere store a exit as a DelayedAccesses pointing
+    to a World attribute rather than a pointer to any particular Somewhere."""
     def __init__(self, mappings=None, **kargs):
         """
         @param mappings: a dictionary that maps strings to Places
@@ -20,25 +20,18 @@ class World(object):
             self._dict = {}
 
     def build(self):
-        """Call this when all Places in this world have been defined, so calculations
-        that depend on Places can take place. trolololo"""
+        """Build all Somewheres in this World."""
         for place in self._dict.values():
             place.build()
 
     def __getitem__(self, key):
-        """Returns a DelayedAccess instance that delay points to the Place associated with key"""
-
-        ###very very temporary
-        return self._dict[key]
-        ###/end
-        
-        #assert key in self._dict
+        """Returns a DelayedAccess instance that delay points to the Somewhere associated with key."""
         return DelayedAccess(lambda: self._dict[key])
 
     def __setitem__(self, key, value):
-        """Map a string to a value, either a Place or a DelayedAccess object."""
-        assert isinstance(key, str)
-        assert isinstance(value, Place) or isinstance(value, DelayedAccess)
+        """Map a string to a Somewhere"""
+        assert isinstance(key, str), key
+        assert isinstance(value, Somewhere), value
         self._dict[key] = value
 
 class DelayedAccess(object):
@@ -58,7 +51,7 @@ class DelayedAccess(object):
         return self.pointer_fn()
 
     @staticmethod
-    def call_or_leave_unchanged(x):
+    def soft_eval(x):
         """Accesses pointer if x is DelayedAccess, otherwise returns x unchanged"""
         if isinstance(x, DelayedAccess):
             return x.get()
@@ -66,19 +59,23 @@ class DelayedAccess(object):
             return x
 
     @staticmethod
-    def eval_list(seq):
+    def soft_eval_list(seq):
         """Maps call_or_leave_unchanged to a sequence, returning a list."""
         [call_or_leavel_unchanged(x) for x in seq]
 
 
-class Place(object):
-    """A Place is somewhere that the player can visit, and which leads to another Place."""
+class Somewhere(object):
+    """A somewhere has a name, can be visited, and leads to Somewhere else.
+    It serves as a superclass for all Places."""
     def __init__(self, name, visit_desc, exit, dialogue_type=SelfPacedDialogue):
         """
-        @param name: a brief string description of this place
-        @param visit_desc: a Dialogue or a Dialogue argument that is displayed when Place is visited
-        @param exit: a Place or a zero arg fn that returns a Place
-        @param dialogue_type: the type of Dialogue that visit_desc is converted to."""
+        @param name: a string moniker for this Somewhere
+        @param visit_desc: a Dialogue or list of strings that is displayed 
+            when this Somewhere is visited. The list of strings in question 
+            can be zero length.
+        @param exit: either a Somewhere, or a zero-arg fn that returns a Somewhere.
+        @param dialogue_type: the type of Dialogue that visit_desc is converted to,
+            should visit_desc be a list."""
         assert isinstance(name, str)
         self.name = name
 
@@ -86,107 +83,110 @@ class Place(object):
             self.visit_desc = visit_desc
         else:
             self.visit_desc = dialogue_type(visit_desc)
+
         if callable(exit):
             self._exit_is_fn = True
             self.exit = exit
-        elif isinstance(exit, Place):
+        elif isinstance(exit, Somewhere):
             self._exit_is_fn = False
             self._exit = exit
         else:
             raise TypeError('{} is invalid exit'.format(exit))
 
     def build(self):
-        """Call this when all Place dependents have been defined."""
+        """Call this when all exits are ready to be accessed."""
+        pass
 
     def visit(self):
-        """Called when player visits this place."""
+        """Called when player visits this Somewhere."""
         self.visit_desc.show_all()
 
     def exit(self):
-        """Called when it's time to leave this place. Returns the next place to be visited."""
-        def check_is_place(x):
-            if not isinstance(x):
-                raise TypeError("{fn} did not return a Place, but returned {x}!".format(
+        """Called when it's time to leave this Somewhere. Returns the next 
+        Somewhere to be visited. Raises an Error if not yet built."""
+        def check_is_somewhere(x):
+            if not isinstance(x, Somewhere):
+                raise TypeError("{fn} did not return a Somewhere, but returned {x}!".format(
                     fn=self._exit, x=x))
             return x
 
         if self._exit_is_fn:
-            return check_is_place(self._exit())
+            return check_is_somewhere(self._exit())
         else:
             return self._exit
-
+    
     @staticmethod
     def test():
-        name = "your house"
-        visit_desc = ["You are ensconced within your middle class home",
-                "Suddenly, a ringing from the doorbell."]
-        c1 = Choice('Exit through the back door',
-                ['Wearing your favorite diamond mocassins, you slip through the backdoor with grace '
-                    'and finesse.', 'You are now at the park.'], loc_the_park)
-        c2 = Choice("Check on the front door to see who's there",
-                ["You tiptoe to the door.",
-                    "Neglecting to peer through the peephole, you unwisely swing open the hinges.",
-                    "FAAAAAAAAACK! It's Proto Krippendorf!"], boy_proto_krippendorf)
-        r = RangeDecision([c1, c2])
-        your_house = Place(name, visit_desc, r.make)
-        your_house.visit()
-        print(your_house.exit())
+        world = World()
+        world['room1'] = Somewhere('room1', ['You are entering room1'], world['room2'])
+        world['room2'] = Somewhere('room2', ['You are entering room2'], world['room1'])
+        world.build()
+        current = world['room1'].get()
+        while True:
+            current.visit()
+            current = current.exit()
 
-class Waypoint(Place):
-    """A Waypoint is a specialized Place with many exits."""
-    def __init__(self, name, visit_desc, exits=[], dialogue_type=SelfPacedDialogue, exit_prompt=None, default_exit_desc=None):
+class Place(Somewhere):
+    """A Place is the standard subclass of Somewhere. A Place can have many potential exits,
+    and lets the Player make the decision of choosing between these exits."""
+    def __init__(self, name, visit_desc, exits=[], decision_prompt=None,
+            make_choice_desc=None, **kargs):
         """
         @param name: name of this place
-        @visit_desc: this is shown when this place is visited
-        @param exits: a list of exits, or (exits, desc_str) tuples. Exits can 
-        be added later using the add_exit() fn, so it is okay to leave this empty.
-        Warning: if not using tuple form, functions as exits will fail unless default_exit_desc is changed.
+        @param visit_desc: this is shown when this place is visited
+        @param exits: a list of valid exits. They can be added later. 
         @param exit_prompt: the prompt shown when the user chooses between exits. Generic default.
-        @param default_exit_desc: the processing fn that is used to make exit choice descriptions 
-        if exits doesn't come in (exit, choice_desc_str, post_choice_str) form. By default, just take the Place name attr."""
-        self.exit_prompt = exit_prompt or "Where will you go?"
-        self.default_exit_desc = default_exit_desc = default_exit_desc or (lambda place: place.name)
+        @param make_choice_text: function that takes in a Somewhere and returns a 
+            one-line description string
+        """
+        self.decision_prompt = decision_prompt or "Where will you go?"
+        self.make_choice_desc = make_choice_desc or (lambda somewhere: somewhere.name)
+        self._decision = RangeDecision([], self.decision_prompt)
+        Somewhere.__init__(self, name, visit_desc, self._decision.make, **kargs)
 
-        self._decision = exit = RangeDecision([], self.exit_prompt)
-        Place.__init__(self, name, visit_desc, exit)
-
+        self._unbuilt_exits = exits
         def build():
-            all_tuples = True
-            all_exits = True
-            for e in exits:
-                if isinstance(e, tuple) or isinstance(e, list):
-                    assert isinstance(e[0], Place) or callable(e)
-                    assert isinstance(e[1], str)
-                    assert isinstance(e[2], str)
-                    all_exits = False
-                else:
-                    assert isinstance(e, Place) or callable(e)
-                    all_tuples = False
-            assert all_tuples ^ all_exits or len(exits) == 0
-
-            if len(exits) == 0:
-                pass
-            elif all_exits:
-                tuples  = [(p, default_exit_desc(p), []) for p in exits]
-            elif all_tuples:
-                tuples  = [(place, default_exit_desc(place), []) for p in exits]
-
-            for e, desc, post_desc in exits:
-                add_exit(Choice(desc, post_desc, followup=e))
+            self._decision.empty()
+            if len(self._unbuilt_exits) == 0:
+                raise TypeError("Cannot build place with no exits!")
+            exits = [DelayedAccess.soft_eval(exit) for exit in self._unbuilt_exits]
+            for exit in exits:
+                desc = self.make_choice_desc(exit)
+                post_desc = []  # okay, so maybe this should be less vapid TODO
+                self._decision.add(Choice(desc, post_desc, followup=exit))
         self.build = build
             
-    def add_exit(self, exit, desc, post_desc):
+    def add_exit(self, exit):
         assert isinstance(exit, Place) or callable(exit)
-        assert isinstance(desc, str)
-        self._decision.add(Choice(desc, post_desc, exit))
+        self._unbuilt_exits.append(exit)
 
     @staticmethod
     def test():
-        name = "your house"
-        visit_desc = ["You are ensconced within your middle class home",
-                "Suddenly, a ringing from the doorbell."]
-        your_house = Waypoint(name, visit_desc, [])
-        for _ in range(10):
-            your_house.add_exit(your_house, your_house.name, [])
-        your_house.visit()
-        print(your_house.exit())
+        world = World()
+        world['room1'] = Place('room1', ['You are visiting room1', 'Rejoice.'])
+        world['room2'] = room2 = Place('room2', ['You are visiting room2', 'Shudder in fear.'])
+        world['room3'] = Place('room3', ['You are visiting room3'], [world['sw1'], world['room2']])
+        world['sw1'] = Somewhere('sw1', ['You visit sw1', 'which leads to room3'], world['room3'])
+        world['sw2'] = Somewhere('sw2', "You visit sw2", world['room1'])
+        green_room = Somewhere('color room', ["You are in the Green Room",
+            "You notice, as you exit this room, that it became a Red Room."], world['room1'])
+        red_room = Somewhere('color room', ["You are in the Red Room",
+            "You notice, as you exit this room, that it became a Green Room."], world['room1'])
+        world['color room'] = green_room
+        room1 = world['room1'].get()
+        room1.add_exit(world['room2'])
+        room1.add_exit(world['room3'])
+        room1.add_exit(world['color room'])
+        room2.add_exit(world['sw1'])
+        world.build()
+
+        current = room1
+        while True:
+            current.visit()
+            if current == red_room:
+                world['color room'] = green_room
+                world.build()
+            elif current == green_room:
+                world['color room'] = red_room
+                world.build()
+            current = current.exit()
