66dfc04cb992c7f65dd92ba35b280eea24368b68
lambdastrife <wang.steven.h@gmail.com>
2015-05-03 22:54:17 -0700

    add text display to datechoose

--- datechoose.py
+++ datechoose.py
@@ -6,7 +6,8 @@ from dialogue import CoolDialogue
 class DateChoose():
 
     def __init__(self, font_name=None, desc_font_size=25, desc_font_color=(255,255,255),
-                 exit_pos=None, bg_img=None):
+                 exit_pos=None, bg_img=None, textbox_pos=(0, 530), textbox_size=(500, 50)
+                 ):
         """Select from 6 tiled pictures of locations where a date can take place. \
         Displays gold cost under each tile. Deducts gold and initiates dating sequence when \
         player clicks on a valid tile."""
@@ -39,10 +40,17 @@ class DateChoose():
             tile.on_hover = on_hover
             tile.on_unhover = on_unhover
         make_tile("Beach of the East", 500, "TILE_beach_east")
-        #make_tile("Beach of the West", 500, "TILE_beach_west")
+        make_tile("Beach of the West", 500, "TILE_beach_west")
+        make_tile("Arcade", 1000, "TILE_arcade")
+        make_tile("Hot Springs", 2000, "TILE_springs")
+
+
+        self.textbox = TextBox("", textbox_pos, textbox_size, frame_color=(230, 40, 0))
+        # TODO: note that frame_color here isn't doing anything.
 
         self.all_sprites = pygame.sprite.Group()
         self.all_sprites.add(self.tiles)
+        self.all_sprites.add(self.textbox)
 
         self.main_surface = pygame.display.get_surface()
         self.done = False
@@ -50,6 +58,8 @@ class DateChoose():
 
     def changeText(self, text):
         """render changed text if necessary"""
+        self.textbox.text = text
+        self.textbox.render()
         print("Text changed to {}.".format(text))
 
     def main_loop(self):
@@ -77,6 +87,8 @@ class DateChoose():
 
             if self.bg_img:
                 self.main_surface.blit(self.bg_img, [0,0])
+            else:
+                self.main_surface.fill((0, 0, 0))
             self.all_sprites.update()
             self.all_sprites.draw(self.main_surface)
             pygame.display.flip()
