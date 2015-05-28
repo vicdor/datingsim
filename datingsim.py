146411b1cfd7709573981f42fa634715a97b5155
lambdastrife <wang.steven.h@gmail.com>
2015-05-28 13:35:31 -0700

    beginning coding datingsim.game_loop()

--- datingsim.py
+++ datingsim.py
@@ -29,11 +29,51 @@ def quit():
     inited = False
     del player
 
+def checkinit():
+    if not inited:
+        raise Error("Game not yet inited!")
+
+def finish():
+    """Requests that the game_loop stops on the next iteration. If currently in \
+        world map, then stops almost immediately. If currently in some scene, \
+        then stops after the current main_loop() exits."""
+    checkinit()
+    global stop_request
+    stop_request = True
+
 def start():
     """Begin the game."""
 
 def snapshot():
     return pygame.display.get_surface().copy()
 
+stack = []
+def add_scene(scene, obliderate_stack=False):
+    """
+    A duck-typed scene is anything with a main_loop() method. Calling add_scene on
+    the provided SCENE will add it to the stack.
+    """
+    global stack
+    if obliderate_stack:
+        stack = []
+    stack.append(scene)
+
+def game_loop():
+    checkinit()
+    global in_game_loop, stop_request, stack
+    in_game_loop = True
+    stop_request = False
+
+    try:
+        while not stop_request:
+            if len(stack) == 0:
+                #TODO: how to open worldmap multiple times
+                #TODO: update worldmap to add Scenes to stack rather than directly calling main_loop
+                pass
+            else:
+                next_scene = stack.pop()
+                next_scene.main_loop()
+    finally:
+        in_game_loop = False
 
 
