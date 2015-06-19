311d5f6732ce839c635cba5726c3d53421c82d54
lambdastrife <wang.steven.h@gmail.com>
2015-06-19 12:18:08 -0700

    semantics

--- kitchen.py
+++ kitchen.py
@@ -15,22 +15,27 @@ def start():
     datingsim.init()
     datingsim.player.inventory.cash = 1000
     from worldmap import WorldMap
-    add_scene(WorldMap())
+    push_scene(WorldMap())
     game_loop()
 
 def snapshot():
     return pygame.display.get_surface().copy()
 
 stack = []
-def add_scene(scene, obliderate_stack=False):
+def push_scene(scene, obliderate_stack=False):
     """
-    A duck-typed scene is anything with a main_loop() method. Calling add_scene on
+    A duck-typed scene is anything with a main_loop() method. Calling push_scene on
     the provided SCENE will add it to the stack.
     """
     global stack
     if obliderate_stack:
         stack = []
     stack.append(scene)
+
+def pop_scene(num=1):
+    global stack
+    for i in range(num):
+        stack.pop()
 
 def game_loop():
     datingsim.checkinit()
@@ -45,7 +50,7 @@ def game_loop():
                 #TODO: update worldmap to add Scenes to stack rather than directly calling main_loop
                 pass
             else:
-                next_scene = stack.pop()
+                next_scene = stack[-1]
                 next_scene.main_loop()
     finally:
         in_game_loop = False
